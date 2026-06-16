6af3d216fa549b10ccc94e22ec2ae8b309442d73
Oluwadarasimi Adeyinka <adeyinkaoluwadarasimi@gmail.com>
2026-06-16 17:09:06 +0100

    edit phone icon in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -111,9 +111,9 @@ text-sm"
             </button>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-10 mb-16">
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:flex lg:justify-around gap-10 lg:gap-20 mb-16">
           {/* Brand column */}
-          <div className="lg:col-span-2">
+          <div className=" lg:col-span-2">
             <Link to="/" className="flex items-center gap-3 mb-5">
               <div className="w-9 h-9 rounded-lg bg-white  flex items-center justify-center">
                 <img src={logo} alt="Logo" />
@@ -132,7 +132,7 @@ text-sm"
             <div className="space-y-3 text-sm text-white/50">
               <div className="flex items-start gap-3">
                 <svg
-                  className="w-4 h-4 text-blue-400 mt-0.5 flex-shrink-0"
+                  className="w-4 h-4 text-blue-400 mt-0 shrink-0"
                   viewBox="0 0 16 16"
                   fill="none"
                 >
@@ -151,16 +151,19 @@ text-sm"
                     d: "M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z",
                     col: "green-500",
                     col2: "#00B328",
+                    viewBox: "0 0 25 25 ",
                   },
                   {
                     num: "+2348142619300",
                     d: "M3 2h3l1.5 3.5-1.75 1.05A10.5 10.5 0 007.45 9.5L8.5 7.75 12 9.25v2.75C12 13.5 10.5 14 9.75 14 4.75 14 2 9.25 2 4.25 2 3.5 2.5 2 4 2z",
                     col: "blue-400",
+                    viewBox: "0 0 16 16",
                   },
                   {
                     num: "+2347045149357",
                     d: "M3 2h3l1.5 3.5-1.75 1.05A10.5 10.5 0 007.45 9.5L8.5 7.75 12 9.25v2.75C12 13.5 10.5 14 9.75 14 4.75 14 2 9.25 2 4.25 2 3.5 2.5 2 4 2z",
                     col: "blue-400",
+                    viewBox: "0 0 16 16",
                   },
                 ].map((phone) => (
                   <div key={phone.num} className=" ">
@@ -169,11 +172,9 @@ text-sm"
                       className=" flex flex-row gap-2 text-white/70 hover:text-white transition-colors text-sm"
                     >
                       <svg
-                        className={`w-5 h-5 text-${phone.col} shrink-0`}
+                        className={`flex  w-5 h-5  justify-center align-middle items-center  text-${phone.col} shrink-0`}
                         fill={phone.col2}
-                        viewBox="0 0 24 24"
-                        // viewBox="0 0 16 16"
-                        // fill="none"
+                        viewBox={phone.viewBox}
                       >
                         <path
                           d={phone.d}
@@ -211,7 +212,7 @@ text-sm"
             </div>
 
             {/* Socials */}
-            <div className="flex gap-3 mt-7">
+            <div className="lg:w-fit flex mx-auto gap-3 lg:gap-7 mt-7">
               {socials.map((s) => (
                 <motion.a
                   key={s.label}
@@ -227,25 +228,27 @@ text-sm"
           </div>
 
           {/* Link columns */}
-          {Object.entries(footerLinks).map(([heading, links]) => (
-            <div key={heading}>
-              <h4 className="text-white text-sm font-semibold mb-5 tracking-wide">
-                {heading}
-              </h4>
-              <ul className="space-y-3">
-                {links.map((link) => (
-                  <li key={link}>
-                    <a
-                      href="#"
-                      className="text-white/40 hover:text-white/80 text-sm transition-colors duration-200"
-                    >
-                      {link}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+          <div className=" lg:w-2xl lg:flex gap-20">
+            {Object.entries(footerLinks).map(([heading, links]) => (
+              <div key={heading} className=" ">
+                <h4 className="text-white text-sm font-semibold mb-2 mt-5 tracking-wide">
+                  {heading}
+                </h4>
+                <ul className="space-y-3">
+                  {links.map((link) => (
+                    <li key={link}>
+                      <a
+                        href="#"
+                        className="text-white/40 hover:text-white/80 text-sm transition-colors duration-200"
+                      >
+                        {link}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
 
         {/* Bottom bar */}
